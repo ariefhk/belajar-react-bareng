bf6a042fb08080469da08fcd8f967d38a0014b50
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-12-08 17:15:58 +0700

    destructure for import module

--- JavaScript Basic/8_async_api_implementation.js
+++ JavaScript Basic/8_async_api_implementation.js
@@ -1,31 +1,33 @@
-import GetAllData from "./Api/GetAllData.js";
-import GetById from "./Api/GetById.js";
-import PostData from "./Api/PostData.js";
-import DeleteData from "./Api/DeleteData.js";
-import PutData from "./Api/PutData.js";
+import {
+  GetAllData,
+  GetById,
+  PostData,
+  DeleteData,
+  PutData,
+} from "./Api/ApiFunctions.js";
 
 //Get All Data
-GetAllData();
+// GetAllData();
 
 //Get Data By Id
 // GetById({
-//   id: "6391a768c923987032825042",
+//   id: "6391a924c923987032825053",
 // });
 
 //Post Data
-// PostData({
-//   title: "Testttt",
-//   desc: "ini test yaa",
-//   published: true,
-// });
+PostData({
+  title: "Mau Post ",
+  desc: "ini test yaa hehe",
+  published: true,
+});
 
 //Delete Data
-// DeleteData({ id: "6391ae4bc92398703282505d" });
+// DeleteData({ id: "6391a8efc923987032825051" });
 
 //Update Data
 // PutData({
 //   id: "6391a924c923987032825053",
-//   title: "Test Update Terbaru",
-//   desc: "ini test update terbaru",
-//   published: false,
+//   title: "Test Update Terbaru5",
+//   desc: "ini test update terbaru5",
+//   published: true,
 // });
---
+++ JavaScript Basic/Api/ApiFunctions.js
@@ -0,0 +1,7 @@
+import GetAllData from "./GetAllData.js";
+import GetById from "./GetById.js";
+import PostData from "./PostData.js";
+import PutData from "./PutData.js";
+import DeleteData from "./DeleteData.js";
+
+export { GetAllData, GetById, PostData, PutData, DeleteData };
